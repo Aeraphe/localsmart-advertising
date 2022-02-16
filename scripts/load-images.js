024588967951e32503a52489788c7e96d9d46d05
Aeraphe <alberto.aeraph@gmail.com>
2022-02-16 13:17:08 -0300

    first commit

---
+++ scripts/load-images.js
@@ -0,0 +1,128 @@
+
+let index = 0;
+
+const start = () => {
+
+    runAdvertisement();
+
+
+
+    setInterval(() => {
+
+        runAdvertisement();
+    }, 5000);
+
+
+    setInterval(() => {
+        transition('visible');
+        setTimeout(() => {
+            transition('hidden');
+        }, 2500);
+
+    }, 20000);
+
+
+}
+
+
+
+const runAdvertisement = () => {
+
+
+    let maxItems = products.length
+    const item = products[index];
+
+    changeDescription(item);
+    changeDetails(item);
+    changeDetails2(item);
+    changeImage(item);
+
+    index += 1;
+
+    if (index == maxItems) {
+
+        index = 0
+    }
+}
+
+
+const transition = (show) => {
+    let transitionEl = document.querySelector('.transition');
+    if (show == 'hidden') {
+        transitionEl.className = "transition "
+    } else {
+        transitionEl.className = "transition transition--active"
+    }
+    transitionEl.style.visibility = show;
+
+
+}
+const changeDescription = (item) => {
+    let producrTile = document.querySelector('#product-tile');
+    let price = document.querySelector('.price__value');
+    let subTilte = document.createElement('div')
+    subTilte.className = "sub-title";
+
+    //Title
+    subTilte.innerText = item.state;
+    producrTile.innerText = item.name;
+    producrTile.appendChild(subTilte);
+    //Price
+    price.innerText = item.price
+}
+
+
+const changeImage = (item) => {
+    let image1 = document.querySelector('.card__img--1');
+    let image2 = document.querySelector('.card__img--2');
+
+    let img1Path = item.imagFolder + item.images['1'].path;
+    let img2Path = item.imagFolder + item.images['2'].path;
+
+    image1.style.backgroundImage = `url('${img1Path}')`;
+    image2.style.backgroundImage = `url('${img2Path}')`;
+}
+
+
+const changeDetails = (item) => {
+
+    let details1 = document.querySelector('#details-1');
+
+
+    let opValue = item.props.fild1[0].value
+    let opName = item.props.fild1[0].name
+
+    let opValue2 = item.props.fild1[1].value
+    let opName2 = item.props.fild1[1].name
+
+    details1.innerHTML = `
+    <li>${opName}:<span class="details-item-description">${opValue}</span></li>
+    <li>${opName2}:<span class="details-item-description">${opValue2}</span></li>
+    `;
+
+
+}
+
+const changeDetails2 = (item) => {
+
+    let details1 = document.querySelector('#details-2');
+
+
+    let opValue = item.props.fild2[0].value
+    let opName = item.props.fild2[0].name
+
+    let opValue2 = item.props.fild2[1].value
+    let opName2 = item.props.fild2[1].name
+
+    details1.innerHTML = `
+    <li>${opName}:<span class="details-item-description">${opValue}</span></li>
+    <li>${opName2}:<span class="details-item-description">${opValue2}</span></li>
+    `;
+
+
+
+}
+
+
+
+start();
---
+++ scripts/products.js
@@ -0,0 +1,57 @@
+
+const defaultPath = "../../assets/photos/";
+const products = [
+
+    {
+
+        name: 'iPhone 12',
+        state: '(Usado)',
+        price: 'R$15.00,00',
+        imagFolder: defaultPath,
+        images: {
+            1: { path: 'teste.jfif' },
+            2: { path: 'iphone13.jfif' }
+        },
+        props: {
+            fild1: [
+                { name: "Modelo", value: "ProMax" },
+                { name: "Bateria", value: "90%" }
+            ],
+            fild2: [
+                { name: "Memoria", value: "64G" },
+                { name: "Câmera", value: "4" }
+            ],
+        }
+    },
+
+    {
+
+        name: 'iPhone 11',
+        state: '(Novo)',
+        price: 'R$11.00,00',
+        imagFolder: defaultPath,
+        images: {
+            1: { path: 'iphone13.jfif' },
+            2: { path: 'iphone12.jpg' }
+        },
+        props: {
+            fild1: [
+                { name: "Modelo", value: "Pro" },
+                { name: "Condição", value: "Usado" }
+            ],
+            fild2: [
+                { name: "Modelo", value: "ProMax" },
+                { name: "Condição", value: "Usado" }
+            ],
+        }
+    }
+
+]
+
+
+
+
+
+
+
+
